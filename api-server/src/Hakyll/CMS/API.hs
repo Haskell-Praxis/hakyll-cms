fb0914faf70188371cdfb740da7072ca4b2d694f
Gabriel Pickl <peacemotion@gmail.com>
2017-06-14 16:05:36 +0200

    Added create request to api.

--- api-server/src/Hakyll/CMS/API.hs
+++ api-server/src/Hakyll/CMS/API.hs
@@ -1,21 +1,29 @@
 {-# LANGUAGE DataKinds     #-}
 {-# LANGUAGE TypeOperators #-}
 
-module Hakyll.CMS.API where
+module Hakyll.CMS.API
+    ( API
+    , PostAPI
+    )
+    where
 
 import           Data.Map
 import           Data.Text
 import           Data.Time
-import           Hakyll.CMS.Types
+import           Hakyll.CMS.Types hiding (Post)
 import qualified Hakyll.CMS.Types as Types
 import           Servant.API
 
 type API =
          ListPosts
-    :<|> Capture "post" Name :> PostAPI
+    :<|> CreatePost
+    :<|> Capture "post" Id :> PostAPI
 
 type ListPosts =
     Get '[JSON] [PostSummary]
+
+type CreatePost =
+    ReqBody '[JSON] NewPost :> Post '[] ()
 
 type PostAPI =
          Get '[JSON] Types.Post
