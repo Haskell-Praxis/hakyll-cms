8d66abd8a429d90a93311e487215ef3c6b2540e6
moru <e1028092@student.tuwien.ac.at>
2017-06-16 17:45:29 +0200

    Copied in router-example.

--- client/Main.hs
+++ client/Main.hs
@@ -1,13 +1,25 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE OverloadedStrings         #-}
--- {-# LANGUAGE RecursiveDo               #-}
+{-# LANGUAGE RecursiveDo               #-}
 {-# LANGUAGE ScopedTypeVariables       #-}
 {-# LANGUAGE TypeFamilies              #-}
+
+
+-- from routing example TODO deletme
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
+
+
+
 
 module Main where
 
 import           Language.Javascript.JSaddle.Warp (run)
 import           Reflex
+import           Reflex                           as R
+import           Reflex.Dom.Old                   (MonadWidget)
 import           Reflex.Dom.Main                  (mainWidgetWithCss)
 import           Reflex.Dom.SimpleMDE
 import           Reflex.Dom.Widget.Basic
@@ -20,11 +32,129 @@ import           Reflex.Dom.Contrib.Router -- we probably only need one of these
 import           Language.Javascript.JSaddle.Types
 import           Control.Monad.Fix
 import           Data.Text                          (Text)
+import           Data.Text                          as T
 
 main :: IO ()
-main = run 8081 $ mainWidgetWithCss simpleMdeCss mainWidget
+-- main = run 8081 $ mainWidgetWithCss simpleMdeCss page
+main = run 8081 $ mainWidgetWithCss simpleMdeCss app
 
-mainWidget :: (
+
+
+-- TODO Use the right constraints in Router module
+type App t m = MonadWidget t m
+
+app :: App t m => m ()
+app = do
+    el "h2" $ text "reflex-dom-contrib examples"
+    el "div" $ routerExample
+    return ()
+
+routerExample :: MonadWidget t m => m ()
+routerExample = do
+    rec r <- partialPathRoute "" . switchPromptlyDyn =<< holdDyn never views
+        views <- dyn $ ffor r $ \case
+            ["A"] -> viewA
+            ["B"] -> viewB
+            ["C"] -> viewC
+            _   -> do
+                text "404!"
+                a <- button "A"
+                b <- button "Back"
+                performEvent_ $  goBack <$ b
+                return ("A" <$ a)
+    return ()
+
+viewA :: MonadWidget t m => m (Event t Text)
+viewA = do
+  el "div" $ text "view A"
+  gotoB <- button "GoTo B"
+  return $ fmap (const "B") gotoB
+
+viewB :: MonadWidget t m => m (Event t Text)
+viewB = do
+  el "div" $ text "view B"
+  gotoC <- button "GoTo C"
+  return $ fmap (const "C") gotoC
+
+viewC :: MonadWidget t m => m (Event t Text)
+viewC = do
+  el "div" $ text "view C"
+  gotoA <- button "GoTo A"
+  return $ fmap (const "A") gotoA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- page :: (
+--     MonadJSM m,
+--     DomBuilder t m,
+--     DomBuilderSpace m ~ GhcjsDomSpace,
+--     TriggerEvent t m,
+--     MonadFix m,
+--     MonadHold t m,
+--     PostBuild t m
+--   ) => m ()
+page :: forall t m. MonadWidget t m => m ()
+page = do
+  el "div" $ text "[title bar?]"
+  el "div" $ routedContent
+  return ()
+
+basePath :: Text
+basePath = ""
+
+routedContent :: forall t m. MonadWidget t m => m ()
+-- routedContent :: (
+--     MonadJSM m,
+--     DomBuilder t m,
+--     DomBuilderSpace m ~ GhcjsDomSpace,
+--     TriggerEvent t m,
+--     MonadFix m,
+--     MonadHold t m,
+--     PostBuild t m
+--
+--     -- MonadWidgetContraints t m
+--
+--   ) => m ()
+routedContent = do
+    rec
+        let routingUpdates = never
+        -- r <- partialPathRoute basePath routingUpdates
+    --     dynText $ fmap T.unlines r
+
+        -- r <- partialPathRoute basePath . switchPromptlyDyn =<< holdDyn never views
+        -- views <- dyn $ ffor r $ pathToView
+    return ()
+
+
+pathToView path = case path of
+  [""] -> text "[ overview over all posts ]"
+  ["new"] -> text "[ new post ]"
+  [postId] -> text "[ edit post ]"
+  _ -> text "[ 404 ]"
+
+postEditView :: (
     MonadJSM m,
     DomBuilder t m,
     DomBuilderSpace m ~ GhcjsDomSpace,
@@ -33,11 +163,26 @@ mainWidget :: (
     MonadHold t m,
     PostBuild t m
   ) => m ()
-mainWidget = do
+postEditView = do
   mdeChangeEvent <- simpleMDEWidget
-  (ct :: Dynamic t Int) <- count mdeChangeEvent
+  (ct :: Dynamic t Int) <- R.count mdeChangeEvent
   display ct
   text ("\n" :: Text)
   dynText =<< holdDyn "" mdeChangeEvent
   -- Reflex.Dom.Builder.Class.DomBuilderSpace m’
   --                with ‘Reflex.Dom.Builder.Immediate.GhcjsDomSpace’
+
+-- either use `Router` and code at https://github.com/reflex-frp/reflex-dom-contrib/issues/28
+-- or use `MonadRouted`
+
+-- Router uses `/` to seperate client-side routing instead of fragment identifiers for some reason
+-- we could use `route'` to strip fragment identifiers before handing the route over to `Router` (and add it when reading from it)
+
+-- i guess monadrouted is the more complex/advanced of the two and we'd need it if we'd do
+-- more complex path manipulation (e.g. relativ path changes, path's processed by nested widgets, etc)
+-- it works on path parts.
+
+-- nvm, both of these use monadwidget (which is deprecated). We might need to write our own routing.
+-- it only appears as constraint though
+-- solution: we could simply see which of the following constraints that make up MonadWidgetConstraint
+--   are necessary to compile the router https://github.com/reflex-frp/reflex-dom/blob/develop/reflex-dom-core/src/Reflex/Dom/Old.hs#L107
