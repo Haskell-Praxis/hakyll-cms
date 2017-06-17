d621be170e94181220e3ebdc0cbcb9ea2c0f25c0
Gabriel Pickl <peacemotion@gmail.com>
2017-06-17 23:01:27 +0200

    Added mock server.

--- Main.hs
+++ Main.hs
@@ -1,26 +1,11 @@
-{-# LANGUAGE NoImplicitPrelude         #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE OverloadedStrings         #-}
-{-# LANGUAGE RecursiveDo               #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
 
 import           ClassyPrelude
-import           Language.Javascript.JSaddle.Warp (run)
+import           Hakyll.CMS.Server
+import           Network.Wai.Handler.Warp
 
-import           Data.FileEmbed
-
-import           Reflex
-import           Reflex.Dom
 
 main :: IO ()
-main = run 8081 $ mainWidgetWithCss importedCss
-  $ do
-    el "h1" (text "A validation demo")
-    -- t <- textInput def
-    -- dynText $ _textInput_value t
-
-importedCss :: ByteString
-importedCss = $(embedFile "./style.css")
+main = run 8080 server
