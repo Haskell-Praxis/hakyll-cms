a056db81a7ba12d1f69130d34b6d479911462f34
moru <e1028092@student.tuwien.ac.at>
2017-06-18 21:02:43 +0200

    More example-posts.

--- api-server/src/Hakyll/CMS/Server.hs
+++ api-server/src/Hakyll/CMS/Server.hs
@@ -46,6 +46,20 @@ postList =
         , content = "Lorem Ipsum 1"
         , date = createDate "2017-01-08"
         }
+    , Types.Post
+        { Types.title = "Hello World"
+        , author = "Sam Dent"
+        , tags = ["tag1", "tag2"]
+        , content = "Vestibulum sed placerat odio. Phasellus pulvinar ex in lorem auctor congue. Maecenas egestas auctor nisl, eget efficitur neque condimentum et. Ut auctor auctor molestie. Vestibulum porta urna sapien, eget iaculis velit bibendum sit amet. Pellentesque venenatis sapien in ligula egestas, eu aliquet orci laoreet."
+        , date = createDate "2017-01-09"
+        }
+    , Types.Post
+        { Types.title = "Getting Started"
+        , author = "Sam Dent"
+        , tags = ["tutorial", "tag2"]
+        , content = "Donec commodo et mauris non tempor. Nam elementum, quam in dapibus bibendum, nisl tortor ornare elit, et viverra lorem erat non velit. Sed in vulputate lorem. Nulla eget metus eu velit pulvinar ullamcorper et a sem. Donec sodales ullamcorper enim sed molestie. Nullam gravida neque dui."
+        , date = createDate "2017-01-10"
+        }
     ]
 
 posts :: Map Id Types.Post
